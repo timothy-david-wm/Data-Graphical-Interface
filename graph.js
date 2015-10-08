59fbf12be775d791ed7f8d640fb7b61875f4beff
David <session2@START-Coding-MBP-C02PP74CG8WN.local>
2015-10-08 16:49:00 -0700

    Top 10 largest cities in america

--- graph.js
+++ graph.js
@@ -1,30 +1,35 @@
-/**
- * Created by session2 on 10/7/15.
- */
 google.load('visualization', '1', {packages: ['corechart', 'bar']});
-google.setOnLoadCallback(drawBasic);
+google.setOnLoadCallback(drawBarColors);
 
-function drawBasic() {
-
-    var data = google.visualization.arrayToDataTable([
-        ['Accedent', 'Amount'],
-        ['NHF', 6],
-        ['HF', 4]
+function drawBarColors() {
+    //noinspection CommaExpressionJS,CommaExpressionJS
+    var data;
+    data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population', {role: 'style'}],
+        ['New York City, NY', 8175000, 'color: blue'],
+        ['Los Angeles, CA', 3792000, 'color: silver'],
+        ['Chicago, IL', 2695000, 'color: brown'],
+        ['Houston, TX', 2099000, 'color: gold'],
+        ['Philadelphia, PA', 1526000, 'color: green'],
+            ['Phoenix, AZ', 1445623, 'color: orange'],
+            ['San antonio, TX', 1327407, 'color: gold'],
+            ['San Diego, CA', 1307402, 'color: silver'],
+            ['Dalas, TX', 1197816, 'color: gold'],
+            ['San jose, CA', 945942, 'color: silver']
     ]);
 
     var options = {
-        title: 'Railroad Accidents',
+        title: 'Population of Largest U.S. Cities',
         chartArea: {width: '50%'},
+        colors: ['#b0120a', '#ffab91'],
         hAxis: {
-            title: 'Accidents',
+            title: 'Total Population',
             minValue: 0
         },
         vAxis: {
-            title: 'Types'
+            title: 'City'
         }
     };
-
     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-
     chart.draw(data, options);
 }
